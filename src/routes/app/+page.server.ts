69e9b6e9a6e59bd0395ed485e98d762073cba4d9
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2025-04-29 10:01:47 -0400

    Starting over

--- src/routes/app/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ locals: { user } }) => {
-	if (user) {
-		redirect(303, '/app/dashboard');
-	}
-};
--- src/routes/app/dashboard/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	let { data } = $props();
-	let { user } = $derived(data);
-</script>
-
-<div class="container mx-auto px-4 py-8">
-	<h1 class="text-3xl font-bold">Dashboard</h1>
-	<p>Logged in user {user?.email ?? 'Unknown'}</p>
-</div>
