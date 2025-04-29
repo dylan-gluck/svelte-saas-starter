7fe2aa1ca18ce0d63c8b71d942ede4d54b681c4e
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2025-04-29 10:38:04 -0400

    Working auth flow

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	let { data, children } = $props();
+	let { supabase } = $derived(data);
+
+	const logout = async () => {
+		const { error } = await supabase.auth.signOut();
+		if (error) {
+			console.error(error);
+		}
+		goto('/auth');
+	};
+</script>
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+	</nav>
+	<button onclick={logout}>Logout</button>
+</header>
+<main>
+	{@render children()}
+</main>
